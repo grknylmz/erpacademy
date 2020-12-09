ec14aad0a399bb6a0a3501b87064a94138e9e7cf
Yilmaz <gurkanyilmaz@kpmg.com>
2020-12-09 10:48:47 +0200

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/Overview.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/kpmg/erpacademy/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.kpmg.erpacademy.controller.Overview", {});
+});
