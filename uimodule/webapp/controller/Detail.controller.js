d1c10e2cf70dbeb80bd15f62f1b3ff5d5be651b6
grknylmz <yilmaz.gurkan@hotmail.com.tr>
2020-12-09 14:25:02 +0200

    users table

---
+++ uimodule/webapp/controller/Detail.controller.js
@@ -0,0 +1,8 @@
+sap.ui.define(
+  ["com/kpmg/erpacademy/controller/BaseController"],
+  function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.kpmg.erpacademy.controller.Detail", {});
+  }
+);
--- uimodule/webapp/controller/Overview.controller.js
+++ uimodule/webapp/controller/Overview.controller.js
@@ -1,7 +1,13 @@
-sap.ui.define([
-  "com/kpmg/erpacademy/controller/BaseController"
-], function(Controller) {
-  "use strict";
+sap.ui.define(
+  ["com/kpmg/erpacademy/controller/BaseController"],
+  function (Controller) {
+    "use strict";
 
-  return Controller.extend("com.kpmg.erpacademy.controller.Overview", {});
-});
+    return Controller.extend("com.kpmg.erpacademy.controller.Overview", {
+      onPress: function () {
+        var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+        oRouter.navTo("detail");
+      },
+    });
+  }
+);
